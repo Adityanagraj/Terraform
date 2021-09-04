b1b97d1375acc128fcfbea5f6b6028643e8fb427
Aditya N <42292430+Adityanagraj@users.noreply.github.com>
2021-09-04 21:35:40 +0530

    Update VM integration with VPC.tf

--- VM integration with VPC.tf
+++ VM integration with VPC.tf
@@ -1,21 +1,32 @@
-#########################################################################################################################
-Here we will be creating our VPC with with our own Subnetwork and attaching it to our OS with Public-IP and Startup Script
-#########################################################################################################################
+#################################################################################################################################################################################
+Here we will be creating our VPC with with our own Subnetwork,Firewall and attaching it to our OS with Public-IP and Startup Script To install apache2 server to host your website
+##################################################################################################################################################################################
 
 provider "google" {
   project     = <project_ID>
   region      = <region>
   credentials = <example.json>
 }
+resource "google_compute_firewall" "firewallforhttpssh" {
+  name    = "firewallforhttpssh"
+  network     = google_compute_network.gravity.name
+  description = "Creates firewall rule targeting tagged instances"
+
+  allow {
+    protocol  = "tcp"
+    ports     = ["80", "22"]
+  }
+  target_tags = ["http","ssh"]
+}
 resource "google_compute_subnetwork" "network-with-private-secondary-ip-ranges" {
   name          = "test-subnetwork"
   ip_cidr_range = "10.2.0.0/16"
   region        = "us-central1"
   network       = google_compute_network.gravity.id
-  secondary_ip_range {
-    range_name    = "tf-test-secondary-range-update1"
-    ip_cidr_range = "192.168.10.0/24"
-  }
+  #secondary_ip_range {
+   # range_name    = "tf-test-secondary-range-update1"
+    #ip_cidr_range = "192.168.10.0/24"
+  #}
 }
 
 resource "google_compute_network" "gravity" {
@@ -29,7 +40,7 @@ resource "google_compute_instance" "tftestos1" {
   zone         = "us-central1-a"
   description = "lauched from tf"
 
-  tags = ["test", "os1"]
+  tags = ["http","ssh","http-server"]
 
   boot_disk {
     initialize_params {
@@ -42,7 +53,7 @@ network_interface {
     access_config {}
 }
 
-metadata_startup_script = "sudo apt update && sudo apt install apache2 && sudo systemctl start apache2 && echo yippe  > /var/www/html/index.html "
+metadata_startup_script = "#!/bin/bash \n sudo apt update && sudo apt install apache2 -y && sudo systemctl start apache2 && echo yippe from terraform  > /var/www/html/index.html "
 depends_on = [
     google_compute_network.gravity,
   ]
